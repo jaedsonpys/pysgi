19967f06220749034548c390d1ebf07e0941b332
Jaedson Silva <contacanal798@gmail.com>
2022-04-13 01:24:34 -0300

    adding request handler file

---
+++ pysgi/_request.py
@@ -0,0 +1,20 @@
+from http_parser.pyparser import HttpParser
+
+
+def parser_http(http_message: str) -> dict:
+    parser = HttpParser()
+    parser.execute(http_message, len(http_message))
+
+    http_content = {}
+
+    http_content['path'] = parser.get_path()
+    http_content['query'] = parser.get_query_string()
+    http_content['method'] = parser.get_method()
+
+    if parser.is_headers_complete():
+        http_content['headers'] = parser.get_headers()
+
+    if parser.is_partial_body():
+        http_content['body'] = parser.recv_body()
+
+    return http_content
