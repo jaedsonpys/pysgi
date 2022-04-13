1c7990d62701f26767d3ef5d1fea09844c3a1dd7
Jaedson Silva <contacanal798@gmail.com>
2022-04-13 11:23:19 -0300

    adding PySGI.route method to add routes to application

--- pysgi/wsgi.py
+++ pysgi/wsgi.py
@@ -1,5 +1,21 @@
-from ._request import parser_http
-from ._sockethandler import SocketHandler
-from .route import Route
+from types import FunctionType
+
+from _request import parser_http
+from _sockethandler import SocketHandler
+from route import Route
 
 socket = SocketHandler()
+routes = {}
+
+class PySGI(object):
+    def route(self, path: str, methods: list = ['GET']) -> FunctionType:
+        route = Route()
+        route.path = path
+        route.allowed_methods = methods
+
+        def _decorator_func(function: FunctionType):
+            route.function = function
+            routes[path] = route
+            return function
+
+        return _decorator_func
