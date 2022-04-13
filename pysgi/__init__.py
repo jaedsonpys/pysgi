3c1db2e5c13d9a1b0d73e7d6aaa04fa91c7978ba
Jaedson Silva <contacanal798@gmail.com>
2022-04-13 17:05:55 -0300

    adding package docstring

--- pysgi/__init__.py
+++ pysgi/__init__.py
@@ -1,3 +1,26 @@
+"""PySGI is a simple and fast server library, giving access
+to all the necessary HTTP information such as headers,
+cookies, requested path and method, and other functionality.
+
+With it, you can easily create complex and fast servers,
+see an example:
+
+```python
+from pysgi import PySGI, make_response
+
+server = PySGI()
+
+
+@server.route('/')
+def index():
+     return make_response('Hello World from PySGI!')
+
+server.run()
+```
+
+Access our official repository and get more information.
+"""
+
 from .wsgi import PySGI
 from .response import make_response
 
