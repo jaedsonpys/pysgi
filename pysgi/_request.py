c61090a6270ea128042a51f2adfc6bad7b632596
Jaedson Silva <contacanal798@gmail.com>
2022-04-13 10:53:27 -0300

    returning object of type ClientRequest in parser_http

--- pysgi/_request.py
+++ pysgi/_request.py
@@ -5,25 +5,6 @@ from typing import Any
 from http_parser.pyparser import HttpParser
 
 
-def parser_http(http_message: str) -> dict:
-    parser = HttpParser()
-    parser.execute(http_message, len(http_message))
-
-    http_content = {}
-
-    http_content['path'] = parser.get_path()
-    http_content['query'] = parser.get_query_string()
-    http_content['method'] = parser.get_method()
-
-    if parser.is_headers_complete():
-        http_content['headers'] = parser.get_headers()
-
-    if parser.is_partial_body():
-        http_content['body'] = parser.recv_body()
-
-    return http_content
-
-
 class ClientRequest(object):
     body: Any
     method: str
@@ -34,3 +15,22 @@ class ClientRequest(object):
     def __repr__(self) -> str:
         return f'ClientRequest(path={self.path}, method={self.method}, \
                 headers={self.headers}, args={self.args}, body={self.body})'
+
+
+def parser_http(http_message: str) -> ClientRequest:
+    parser = HttpParser()
+    parser.execute(http_message, len(http_message))
+
+    client_request = ClientRequest()
+
+    client_request.path = parser.get_path()
+    client_request.args = parser.get_query_string()
+    client_request.method = parser.get_method()
+
+    if parser.is_headers_complete():
+        client_request.headers = parser.get_headers()
+
+    if parser.is_partial_body():
+        client_request.body = parser.recv_body()
+
+    return client_request
