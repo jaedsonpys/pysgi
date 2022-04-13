607f9cddbc6de0452ca393e9ed0bb4120fa2d1c5
Jaedson Silva <contacanal798@gmail.com>
2022-04-13 11:28:13 -0300

    adding exception throwing in PySGI.route()

--- pysgi/wsgi.py
+++ pysgi/wsgi.py
@@ -10,6 +10,9 @@ routes = {}
 
 class PySGI(object):
     def route(self, path: str, methods: list = ['GET']) -> FunctionType:
+        if not isinstance(methods, list):
+            raise TypeError(f'A named list of methods is required, not {type(methods)}')
+
         route = Route()
         route.path = path
         route.allowed_methods = methods
