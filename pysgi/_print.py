c5d95adf4710f68a08f6c95dc794ea36d86a0e80
Jaedson Silva <contacanal798@gmail.com>
2022-04-14 00:46:01 -0300

    adding print_request function

--- pysgi/_print.py
+++ pysgi/_print.py
@@ -5,3 +5,8 @@ from .__init__ import __version__
 def print_start(host: str, port: int) -> None:
     print(f'\033[1m@PySGI Server ({__version__})\033[m')
     print(f'\033[4;47;35mRunning in http://{host}:{port}\033[m\n')
+
+
+def print_request(path: str, method: str, host: str) -> None:
+    time = datetime.now()
+    print(f'   * [{time}] {host}: ({method} {path})')
