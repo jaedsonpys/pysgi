43f746fda409adcfaf9ae20bc6210e57e2af0ac6
Jaedson Silva <contacanal798@gmail.com>
2022-04-14 01:29:40 -0300

    adding initial project setup information

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from .pysgi import __version__
+
+setup(
+    name='PySGI',
+    version=__version__,
+    description='The easy server library for everyone.',
+    author='Jaedson Silva',
+    author_email='imunknowuser@protonmail.com',
+    packages=['pysgi'],
+    install_requires=['http-parser'],
+    license='MIT License'
+)
