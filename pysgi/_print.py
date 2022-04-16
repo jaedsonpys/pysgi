906bb0e2b4cb2f224afd294ea0515ee3d7667b79
Jaedson Silva <contacanal798@gmail.com>
2022-04-16 16:32:08 -0300

    removing TABS and fixing conditions in _print.py

--- pysgi/_print.py
+++ pysgi/_print.py
@@ -8,8 +8,7 @@ def print_start(host: str, port: int) -> None:
 
 def print_response(status: int, path: str, method: str, host: str) -> None:
     time = datetime.now()
-    print(' ' * 4, end='')
 
-    if status >= 400: print(f'* [{time}][\033[1;31m{status}\033[m] {host}: ({method}) {path}')
-    if status >= 300: print(f'* [{time}][\033[1;33m{status}\033[m] {host}: ({method}) {path}')
-    if status >= 200: print(f'* [{time}][\033[1;32m{status}\033[m] {host}: ({method}) {path}')
+    if status >= 400:print(f'* [{time}][\033[1;31m{status}\033[m] {host}: ({method}) {path}')
+    elif status >= 300: print(f'* [{time}][\033[1;33m{status}\033[m] {host}: ({method}) {path}')
+    elif status >= 200: print(f'* [{time}][\033[1;32m{status}\033[m] {host}: ({method}) {path}')
