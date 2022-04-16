941c17b4a1925962aaf81a9f210ab72a4dce65eb
Jaedson Silva <contacanal798@gmail.com>
2022-04-15 22:33:08 -0300

    adding methods docstring

--- pysgi/wsgi.py
+++ pysgi/wsgi.py
@@ -11,6 +11,22 @@ routes = {}
 
 class PySGI(object):
     def route(self, path: str, methods: list = ['GET']) -> FunctionType:
+        """Adds a new route to the application.
+
+        Pass the "route" argument with the name of
+        the route (using "/") and also pass the
+        "methods" argument, which is a list.
+        In the "methods" argument, you define
+        the methods accepted by the route, 
+        such as `GET`, `POST`, `PUT` and others.
+
+        :param path: Route name.
+        :type path: str
+        :param methods: Accepted methods, defaults to ['GET']
+        :type methods: list, optional
+        :raises TypeError: If the `methods` argument is not a list
+        """
+
         if not isinstance(methods, list):
             raise TypeError(f'A named list of methods is required, not {type(methods)}')
 
@@ -26,6 +42,19 @@ class PySGI(object):
         return _decorator_func
 
     def run(self, host: str = None, port: str = None) -> None:
+        """Starts the server on the specified host
+        and port (or not).
+
+        It is in this method that the server
+        starts waiting for connections and
+        starting threads to handle requests.
+
+        :param host: _description_, defaults to '127.0.0.1'
+        :type host: str, optional
+        :param port: _description_, defaults to 5500
+        :type port: str, optional
+        """
+
         address = _server.create_socket(host=host, port=port)
         request = Request(routes)
 
