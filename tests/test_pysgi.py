3b02466e4331bb6428d500f58ebc3ae4cf599401
Jaedson Silva <contacanal798@gmail.com>
2022-04-16 20:13:59 -0300

    adding test of "make_response()" function

--- tests/test_pysgi.py
+++ tests/test_pysgi.py
@@ -1,5 +1,6 @@
 import unittest
 from pysgi import Response
+from pysgi.response import make_response
 
 
 class TestPySGI(unittest.TestCase):
@@ -9,6 +10,21 @@ class TestPySGI(unittest.TestCase):
         response.set_header('Authorization', 'Bearer 28082044d2')
 
         self.assertEqual(response.cookies, {'authJWT', 'ey2808'}, msg='Expected cookies not equal')
+
+    def test_make_response(self):
+        expected_message = 'HTTP/1.1 200\n' \
+                           'Server: PySGI\n' \
+                           'Content-Type: text/html\n' \
+                           'Authorization: Bearer 28082044d2\n' \
+                           'Set-Cookie: authJWT=ey2808\n\n' \
+                           'Hello'
+
+        response = Response('Hello', status=200)
+        response.set_cookie('authJWT', 'ey2808')
+        response.set_header('Authorization', 'Bearer 28082044d2')
+
+        http_message = make_response(response)
+        self.assertEqual(http_message, expected_message, msg='Expected HTTP message not equal')
 
 
 if __name__ == '__main__':
