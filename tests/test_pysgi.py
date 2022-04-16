1d3fc9a2eb6a01c623f2766508a3ebce25b0fd0d
Jaedson Silva <contacanal798@gmail.com>
2022-04-16 20:03:00 -0300

    updating unit test

--- tests/test_pysgi.py
+++ tests/test_pysgi.py
@@ -1,24 +1,14 @@
-import sys
 import unittest
+from pysgi import Response
 
-sys.path.insert(0, './')
-
-from pysgi.response import make_response
 
 class TestPySGI(unittest.TestCase):
-    def test_make_response(self):
-        headers = {'Content-Type': 'text/plain', 'Auth': '120h'}
-        cookies = {'authJwt': 'ey2723base', 'sessionId': '2324'}
+    def test_response(self):
+        response = Response('Hello', status=200)
+        response.set_cookie('authJWT', 'ey2808')
+        response.set_header('Authorization', 'Bearer 28082044d2')
 
-        response = make_response('Hello', status=200, headers=headers, cookies=cookies)
-        expected_response = 'HTTP/1.1 200\n'\
-                            'Server: PySGI\n'\
-                            'Content-Type: text/plain\n'\
-                            'Auth: 120h\n'\
-                            'Set-Cookie: authJwt=ey2723base; sessionId=2324\n\n'\
-                            'Hello'
-
-        self.assertEqual(response, expected_response, msg='Expected response not equal')
+        self.assertEqual(response.cookies, {'authJWT', 'ey2808'}, msg='Expected cookies not equal')
 
 
 if __name__ == '__main__':
