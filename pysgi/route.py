d9eec9a8a216db8e93c5cc515f857699f55dd23c
Jaedson Silva <contacanal798@gmail.com>
2022-04-23 16:33:47 -0300

    adding "parameter" attribute in Route class and correcting __repr__ string

--- pysgi/route.py
+++ pysgi/route.py
@@ -4,8 +4,9 @@ from types import FunctionType
 class Route(object):
     function: FunctionType
     allowed_methods: list
+    parameters: list = []
     path: str
 
     def __repr__(self) -> str:
-        return f'Route(function={self.function}, allowed_methods={self.allowed_methods}\
-                path={self.path})'
+        return f'Route(function={self.function}, allowed_methods={self.allowed_methods}, ' \
+               f'path={self.path}, dynamics={self.dynamics})'
