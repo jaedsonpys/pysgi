394ae8a22a329dc940f4ede548f6db8dba26374d
Jaedson Silva <contacanal798@gmail.com>
2022-04-25 21:28:58 -0300

    updating setup.py project version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='PySGI',
-    version='1.1.0',
+    version='1.2.0',
     description='The easy server library for everyone.',
     author='Jaedson Silva',
     author_email='imunknowuser@protonmail.com',
