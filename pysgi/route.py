b191601caf3340c1d72006a80aac81d15b064577
x07exploit <0x7ex@protonmail.com>
2022-07-05 11:49:30 -0400

    Update route.py

--- pysgi/route.py
+++ pysgi/route.py
@@ -1,6 +1,5 @@
 from types import FunctionType
 
-
 class Route(object):
     def __init__(self) -> None:
         self.function: FunctionType = None
@@ -10,5 +9,25 @@ class Route(object):
         self.path: str = None
 
     def __repr__(self) -> str:
-        return f'Route(function={self.function}, allowed_methods={self.allowed_methods}, ' \
-               f'path={self.path}, parameters={self.parameters}, no_parameters={self.no_parameters})'
+        """
+        @return: str
+        """
+        return (
+            f"Route(function={self.function}, allowed_methods={self.allowed_methods}, " \
+            f"path={self.path}, parameters={self.parameters}, no_parameters={self.no_parameters})"
+        )
+
+    def __eq__(self, other: object) -> bool:
+        """
+        @param other: object
+        @return: bool
+        """
+        if isinstance(other, Route):
+            return (
+                self.function == other.function and
+                self.allowed_methods == other.allowed_methods and
+                self.path == other.path and
+                self.parameters == other.parameters and
+                self.no_parameters == other.no_parameters
+            )
+        return False
