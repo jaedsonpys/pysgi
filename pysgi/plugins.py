3b297dd70f7cd8dda3ebaee69b6f75eb901469b7
x07exploit <0x7ex@protonmail.com>
2022-07-05 12:00:26 -0400

    Update plugins.py

--- pysgi/plugins.py
+++ pysgi/plugins.py
@@ -1,20 +1,18 @@
 from types import FunctionType
 from functools import wraps
 
-
 class IPFilter(object):
-    _permitted = []
+    _permitted: list = []
 
     def add_permitted(self, ip: str) -> None:
         self._permitted.append(ip)
 
-    def ipfilter(self, function: FunctionType) -> FunctionType:
-        @wraps(function)
-        def wrapper(*args):
+    def ipfilter(self, func: FunctionType) -> FunctionType:
+        @wraps(func)
+        def wrapper(*args, **kwargs):
             request = args[0]
             if request.host in self._permitted:
-                return function(*args)
+                return func(*args, **kwargs)
             else:
-                return 'Forbidden', 401
-
+                return "Unauthorized", 401
         return wrapper
