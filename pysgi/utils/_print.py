e8fdac4567b5585253e3fd57992922ea58da4d70
Jaedson Silva <contacanal798@gmail.com>
2022-09-15 21:05:52 -0300

    changing start log message and response log

--- pysgi/utils/_print.py
+++ pysgi/utils/_print.py
@@ -1,18 +1,22 @@
 from datetime import datetime
 
 
-def print_start(host: str, port: int) -> str:
-    print(f'\033[1m@PySGI Server\033[m')
-    print(f'\033[4;47;35mRunning in http://{host}:{port}\033[m\n')
+def print_start(host: str, port: int) -> None:
+    print('\033[4;32mPySGI application server started\033[m\n')
+    print('»» Learn more about the server at \033[4mgithub.com/jaedsonpys/pysgi\033[m')
+    print(f'»» Running in http://{host}:{port}\n')
 
 
-def print_response(status: int, path: str, method: str, host: str) -> str:
+def print_response(status: int, path: str, method: str, host: str) -> None:
     time = datetime.now()
+
     status_code = {
-        400: f'* [{time}][\033[1;31m{status}\033[m] {host}: ({method}) {path}',
-        300: f'* [{time}][\033[1;33m{status}\033[m] {host}: ({method}) {path}',
-        200: f'* [{time}][\033[1;32m{status}\033[m] {host}: ({method}) {path}',
+        400: f'» [\033[33m{time}\033[m][{host}] \033[1;31m{status}\033[m {path} ({method})',
+        300: f'» [\033[33m{time}\033[m][{host}] \033[1;33m{status}\033[m {path} ({method})',
+        200: f'» [\033[33m{time}\033[m][{host}] \033[1;32m{status}\033[m {path} ({method})',
     }
+
     for key, value in status_code.items():
         if status >= key:
             print(value)
+            break
