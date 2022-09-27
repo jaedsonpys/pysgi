cb371432ad718a4d0178780e5f2bc0ea83afd764
Jaedson Silva <contacanal798@gmail.com>
2022-09-27 19:47:27 -0300

    updating project version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('DESCRIPTION.md', 'r') as reader:
 
 setup(
     name='PySGI',
-    version='1.3.2',
+    version='1.3.3',
     description='The easy server library for everyone.',
     long_description=description,
     long_description_content_type='text/markdown',
